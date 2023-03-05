80a4f99ad5ce543f15034f696963c554ebbd5789
Amyr Francisco <amyrfrancisco@MacBook-Air.local>
2023-03-05 16:23:56 +0800

    changed version to 2

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.write('# Calculator')
+st.write('# Calculator V2.0')
 first = st.number_input('Insert first number')
 second = st.number_input('Insert second number')
 sum = first + second
