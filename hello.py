98743cfd1c9e1f0a7a782cce68f11d3822391db2
Amyr Francisco <amyrfrancisco@MacBook-Air.local>
2023-03-05 17:59:48 +0800

    added columns

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 st.write('# Calculator V2.0')
-first = st.number_input('Insert first number')
-second = st.number_input('Insert second number')
+col1, col2 = st.columns([1, 1])
+first = col1.number_input('Insert first number')
+second = col2.number_input('Insert second number')
 sum = first + second
 
 st.write(f'## = {sum}')
